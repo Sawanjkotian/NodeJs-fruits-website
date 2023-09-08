c42033c0ef3c5438bb0ba325dcabc74611fda93b
Sawanjkotian <sawan9kotian@gmail.com>
2023-09-08 17:07:22 +0530

    First Nodejs Completely

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
+
 //Reading and writing using Nodejs
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//Used to map a string to the url
+const slugify = require('slugify');
+
+const replaceTemplate = require('./module/replaceTemplate')
 
 //====================FILES==================================================
 //CodeBlockig and Synchronous way
@@ -39,29 +45,22 @@ console.log('Reading file.....');
 
 //====================SERVER==================================================
 
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
-
+//Synchronous loading of the Pages at first so that it need not be called again and again when the request is made
 const tempOverview = fs.readFileSync('./final/templates/template-overview.html','utf-8');
 const tempProduct = fs.readFileSync('./final/templates/template-product.html','utf-8');
 const tempCard = fs.readFileSync('./final/templates/template-card.html','utf-8');
 const data = fs.readFileSync('./final/dev-data/data.json','utf-8');
 
+//Creates an array of the json objects named dataObj
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el=> slugify(el.productName, {lower: true}));
+console.log(slugs);
+//Turning it to lower case
+//console.log(slugify('Fresh Avacados', {lower: true}));
+
 const server = http.createServer((req, res)=>{
+
 
     const { query, pathname } = url.parse(req.url, true);
     
